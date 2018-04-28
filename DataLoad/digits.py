63ab943034d957319f19737a671ec3f109aaf05c
Matthew Yeager <mpyeager@icloud.com>
2018-04-28 14:21:44 +0100

    Bug fix

--- DataLoad/digits.py
+++ DataLoad/digits.py
@@ -5,7 +5,8 @@ from sklearn import datasets
 
 # To load [sample] dataset below, uncomment desired dataset
 # Comment out datasets not being tested, used
-digits = digits.load_digits() #1,797 images of handwritten digits, good for teaching image classification 
+digits = digits.load_digits()
+#1,797 images of handwritten digits, good for teaching image classification
 #boston = datasets.load_boston() #503 Boston housing prices, good for exploring regression algos
 #iris = datasets.load_iris() #150 measurements of Iris flowers, good for exploring classification algos
 
